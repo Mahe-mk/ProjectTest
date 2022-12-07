cba1b28cd4c758c7a05c42ab6dad88d768ae5b48
Mahe-mk <mahemk07@gmail.com>
2022-12-07 16:39:59 +0530

    Add initial of the user

--- office/employee/views.py
+++ office/employee/views.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your views here.
 
 def empDetails(request):
-    name='Mahesh'
+    name='Mahesh.V'
     emp_data = employeedetail.objects.all()
     emp_dict = {'emp_list': emp_data}
    # context.update({'emp_list': emp_data})
